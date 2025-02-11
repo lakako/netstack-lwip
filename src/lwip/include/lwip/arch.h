ea0881275a52be9005d6a5fcd1956d701b66b9ae
lakako <46197434+lakako@users.noreply.github.com>
2025-02-11 22:55:48 +0800

    Update arch.h

--- src/lwip/include/lwip/arch.h
+++ src/lwip/include/lwip/arch.h
@@ -199,7 +199,9 @@ typedef uintptr_t mem_ptr_t;
 #include <unistd.h>
 #endif
 #else /* SSIZE_MAX */
-#ifndef ssize_t
+#ifdef __FreeBSD__
+#include <sys/types.h> 
+#else
 typedef int ssize_t;
 #endif
 #define SSIZE_MAX INT_MAX
